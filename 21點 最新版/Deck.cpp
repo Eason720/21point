9b38d7d78f171aa62c0f36c2fa89df755b580022
Eason720 <165125250+Eason720@users.noreply.github.com>
2024-06-18 16:34:16 +0800

    Update Deck.cpp

--- 21點 最新版/Deck.cpp
+++ 21點 最新版/Deck.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <stdexcept>
+using namespace std;
 
 Deck::Deck() {
     for (int i = 1; i <= 10; ++i) {
@@ -14,13 +15,13 @@ Deck::Deck() {
 }
 
 void Deck::shuffleDeck() {
-    std::srand(std::time(0));
-    std::random_shuffle(cards.begin(), cards.end());
+    srand(time(0));
+    random_shuffle(cards.begin(), cards.end());
 }
 
 Card Deck::drawCard() {
     if (cards.empty()) {
-        throw std::out_of_range("No more cards in the deck.");
+        throw out_of_range("No more cards in the deck.");
     }
     Card drawn = cards.back();
     cards.pop_back();
